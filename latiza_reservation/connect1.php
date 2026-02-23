4b7e3324f49f6e89b2414be9f6a9beaf9b835af2
Zita Ruzsinszki <zita.ruzsinszki@gmail.com>
2026-02-23 17:10:16 +0100

    Update connect1.php

--- latiza_reservation/connect1.php
+++ latiza_reservation/connect1.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "a056um.forpsi.com"; // Az adatbázis szerverének neve (általában "localhost")
-$username = "b26544"; // Az adatbázis felhasználóneve (alapértelmezetten "root" XAMPP esetén)
-$password = "tizacska"; // Az adatbázis jelszava (alapértelmezetten üres XAMPP esetén)
-$database = "b26544"; // Az adatbázis neve, amit létrehoztál
+
 
 // Kapcsolat létrehozása és ellenőrzése
 $conn = new mysqli($servername, $username, $password, $database);
@@ -16,4 +13,5 @@ $conn = new mysqli($servername, $username, $password, $database);
 
 // Kapcsolat bezárása
 //$conn->close();
+
 
